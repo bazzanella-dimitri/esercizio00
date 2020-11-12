4e375523ce300468f107c01f6475701337ce831d
bazzanella-dimitri <bazzanelladimitri@gmail.com>
2020-11-12 17:14:20 +0100

    finisco 2.2

---
+++ Esercizi Blocco 2/2.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero; // inizializzo numero
+    int sum=1; // inizializzo sommatoria a 1 
+
+    // faccio ciclo for di 10  cicli, aumentando ad ogni ciclo i di 1 
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Dammi un numero: " << endl;
+        cin >> numero;
+
+        sum+=numero; // la somma equivale a somma + il numero messo per ogni ciclo 
+
+    }
+    cout << "La somma dei numeri equivale a: " << sum << endl;
+    return 0;
+}
---
+++ Esercizi Blocco 2/2.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+  
+
+int main() {
+
+    int a[i];
+
+     for (int a[i]; i < count; i++)
+     {
+         
+     }
+     
+}
